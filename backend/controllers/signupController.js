417b0525b9e09936e26ef95332040b52d756f056
santiago <santiagodeandrade4@gmail.com>
2025-06-21 14:07:59 -0400

    Solucion a errores para finalizar registro de clientes y proveedores

--- backend/controllers/signupController.js
+++ backend/controllers/signupController.js
@@ -9,7 +9,7 @@ exports.signupUser = async (req, res) => {
       await db.pool.query('SELECT create_proveedor($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)', [razon_social, denominacion, rif, url_web, email, password, id_lugar, direccion_fisica, id_lugar2, direccion_fiscal]);
     } else if (tipo_usuario === 'cliente_juridico') {
       const { razon_social, rif, denominacion_comercial, capital_disponible, pagina_web, id_lugar, id_lugar2, direccion_fisica, direccion_fiscal } = req.body;
-      await db.pool.query('SELECT create_cliente_juridico($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)', [rif, razon_social, denominacion_comercial, capital_disponible, direccion_fiscal, direccion_fisica, pagina_web, id_lugar, id_lugar2, email, password]);
+      await db.pool.query('SELECT create_cliente_juridico($1,$2,$3,$4,$5,$6,$7,$8,$9,$10, $11)', [rif, razon_social, denominacion_comercial, capital_disponible, direccion_fiscal, direccion_fisica, pagina_web, id_lugar, id_lugar2, email, password]);
     } else if (tipo_usuario === 'cliente_natural') {
       const { cedula, rif, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, id_lugar, direccion_fisica } = req.body;
       await db.pool.query('SELECT create_cliente_natural($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)', [cedula, rif, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, email, password, id_lugar, direccion_fisica]);
