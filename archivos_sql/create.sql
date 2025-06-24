9c9ab51b48346815eee35778e034b9a6c74d8918
santiago <santiagodeandrade4@gmail.com>
2025-06-24 16:22:59 -0400

    Modificaciones para manejo de roles

--- archivos_sql/create.sql
+++ archivos_sql/create.sql
@@ -435,6 +435,7 @@ CREATE TABLE Rol_Privilegio (
     id_rol          INTEGER NOT NULL,
     id_privilegio   INTEGER NOT NULL,
     fecha_asignacion DATE NOT NULL,
+    nom_tabla_ojetivo VARCHAR(25) NOT NULL,
     motivo          TEXT,
     PRIMARY KEY (id_rol, id_privilegio)
 );
