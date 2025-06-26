7fdca2e62af629f2fc48a76a29e3d8c0d8dd1618
santiago <santiagodeandrade4@gmail.com>
2025-06-26 10:26:15 -0400

    Adicion de validaciones para insercion de rifs y cedulas

--- archivos_sql/functions.sql
+++ archivos_sql/functions.sql
@@ -115,7 +115,7 @@ DECLARE
   new_proveedor_id INTEGER;
   new_usuario_id INTEGER;
 BEGIN
-  INSERT INTO Proveedor (razon_social, denominacion, rif, url_web, id_lugar, direccion_fisica, id_lugar2, direccion_fiscal)
+  INSERT INTO Proveedor (razon_social, denominacion, rif, url_web, id_lugar, direccion_fisica, lugar_id2, direccion_fiscal)
   VALUES (p_razon_social, p_denominacion, p_rif, p_url_web, p_id_lugar, p_direccion_fisica, p_id_lugar2, p_direccion_fiscal)
   RETURNING id_proveedor INTO new_proveedor_id;
 
@@ -123,8 +123,8 @@ BEGIN
   VALUES (new_proveedor_id,  CURRENT_DATE, p_contrasena)
   RETURNING id_usuario INTO new_usuario_id;
 
-  INSERT INTO Correo (nombre, extension_pag, id_usuario)
-  VALUES (split_part(p_correo, '@', 1), split_part(p_correo, '@', 2), new_usuario_id);
+  INSERT INTO Correo (nombre, extension_pag, id_proveedor_proveedor)
+  VALUES (split_part(p_correo, '@', 1), split_part(p_correo, '@', 2), new_proveedor_id);
 END;
 $$ LANGUAGE plpgsql; 
 
